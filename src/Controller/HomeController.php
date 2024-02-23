19e8d078bb0247d1f74f000bf8ff1f66194de182
Thibaut LAHAIE <thibaut.lahaie@etu.univ-amu.fr>
2024-02-23 10:07:02 +0100

    edit homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,6 +25,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home_fruit', ['nomFruit' => 'watermelon']);
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'musiqueAleatoire' => null,
